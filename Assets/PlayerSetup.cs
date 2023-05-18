c430021396404963f7b31e5b677421c413787b23
Juan José <67957125+JJaoMein@users.noreply.github.com>
2023-05-18 11:45:00 -0500

    Project Upload

---
+++ Assets/PlayerSetup.cs
@@ -0,0 +1,31 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerSetup : MonoBehaviour
+{
+    public Movement movement;
+    public GameObject camera;
+
+    public string nickname;
+
+    public TextMeshPro nicknameText;
+
+    public void IslocalPlayer()
+    {
+        movement.enabled = true;
+        camera.SetActive(true);
+
+    }
+
+    [PunRPC]
+    public void setNickname(string _nickname)
+    {
+        nickname = _nickname;
+
+        nicknameText.text = nickname;
+    }
+
+}
---
+++ Assets/RoomManager.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.Linq.Expressions;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public static RoomManager instance;
+
+    public GameObject player;
+
+    [Space]
+    public Transform [] spawnPoints;
+
+    [Space]
+    public GameObject roomCam;
+
+    private string nickname = "Unnammed";
+
+    [Space]
+    public GameObject nameUI;
+    public GameObject connectingUI;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void JoinRoomButton()
+    {
+        Debug.Log("Conecting...");
+
+        PhotonNetwork.ConnectUsingSettings();
+
+        nameUI.SetActive(false);
+        connectingUI.SetActive(true);
+    }
+
+    void Start()
+    {
+
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+
+        Debug.Log("Connected to Server");
+
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+
+        Debug.Log("we're in a lobby");
+
+        PhotonNetwork.JoinOrCreateRoom("test",null,null);
+
+
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+
+        Debug.Log("Connected and in a room");
+
+        roomCam.SetActive(false);
+
+        SpawnPlayer();
+    }
+
+    public void SpawnPlayer()
+    {
+
+        Transform spawnPoint = spawnPoints[UnityEngine.Random.Range(0, spawnPoints.Length)];
+        GameObject _player = PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity);
+        _player.GetComponent<PlayerSetup>().IslocalPlayer();
+        _player.GetComponent<Health>().isLocalPlayer = true;
+
+        _player.GetComponent<PhotonView>().RPC("setNickname", RpcTarget.AllBuffered, nickname);
+
+    }
+
+    public void PickNickname(string _nickname)
+    {
+        nickname= _nickname;
+    }
+
+
+
+
+}
